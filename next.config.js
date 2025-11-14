8809909d9f722c6d9e02f6d74c99c1fea906328d
Luka Kalajžić <112662366+lkalajzic@users.noreply.github.com>
2025-11-14 15:51:26 +0100

    fix: add Clarity and GA4 domains to CSP (*.clarity.ms wildcard)

--- next.config.js
+++ next.config.js
@@ -9,9 +9,9 @@ const nextConfig = {
             key: 'Content-Security-Policy',
             value: [
               "default-src 'self'",
-              "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://www.googletagmanager.com https://www.google-analytics.com https://connect.facebook.net",
-              "img-src 'self' data: https://www.googletagmanager.com https://www.google-analytics.com https://www.facebook.com",
-              "connect-src 'self' https://www.google-analytics.com https://www.facebook.com",
+              "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://www.googletagmanager.com https://www.google-analytics.com https://connect.facebook.net https://*.clarity.ms",
+              "img-src 'self' data: https://www.googletagmanager.com https://www.google-analytics.com https://www.facebook.com https://*.clarity.ms https://fonts.gstatic.com",
+              "connect-src 'self' https://www.google-analytics.com https://region1.google-analytics.com https://www.facebook.com https://*.clarity.ms",
               "frame-src https://www.googletagmanager.com",
               "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com",
               "font-src 'self' https://fonts.gstatic.com",
